c76cb51a60a9711515c6fb4a5319f259d404bb3e
Mvideo <e.sandimirov@yandex.ru>
2025-04-14 16:17:43 +0300

    added TextBox

---
+++ src/test/java/locator/CssXpathExamples.java
@@ -0,0 +1,9 @@
+package locator;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class CssXpathExamples {
+    void cssXpathExamples (Object $setValue) {
+        $("").setValue("1");
+    }
+}
